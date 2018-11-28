806e4cb343bcc8280d73b6adce23d55de05a49f4
王正东 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comdhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-11-28 09:31:37 +0800

    API测试

--- modules/admin/cashier/orders/refund/apply_module.class.php
+++ modules/admin/cashier/orders/refund/apply_module.class.php
@@ -237,7 +237,7 @@ class admin_cashier_orders_refund_apply_module extends api_admin implements api_
         if (is_ecjia_error($result)) {
             //该订单撤销正在处理中，请稍候；钱已退；但退款状态未更新
             if ( $result->get_error_code() == 'pay_wait_manual_confirm') {
-                $find_result = (new Ecjia\App\Payment\Query\FindManager($order_info['order_sn']))->find();
+                $find_result = (new Ecjia\App\Payment\Query\FindManager($order_sn))->find();
 //                             	if (is_ecjia_error($find_result)) {
 //                             		return $result;
 //                             	}
