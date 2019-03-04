c2792491b891321a713910e7b69508edf1125228
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-03-04 17:24:41 +0800

    提交删除店铺收银台设备

--- apis/cashier_store_remove_cleardata_api.class.php
+++ apis/cashier_store_remove_cleardata_api.class.php
@@ -65,6 +65,7 @@ class cashier_store_remove_cleardata_api extends Component_Event_Api
 
         return [
             new \Ecjia\App\Cashier\StoreCleanHandlers\StoreCashierPendorderClear($store_id),
+            new \Ecjia\App\Cashier\StoreCleanHandlers\StoreCashierDeviceClear($store_id),
         ];
     }
 
